10e65565a8e6b892802c640115d90e57148c492c
Gunjan <gunjankohli2004@gmail.com>
2024-11-05 13:26:23 +0530

    user register

---
+++ client/public/server.js
@@ -0,0 +1,59 @@
+//Framework Configuration
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+const hbs = require("hbs");
+const path = require("path");
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandler);
+
+// ERROR handling middleware
+app.use(errorHandler);
+
+app.set('view engine', 'hbs');
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+
+
+// APP CONFIG START
+app.listen(port, () =>{
+    console.log(Server running in port http://localhost:${port});
+});
