fe443455cfa553aba6d1ef8d1e01483bc45f00d2
Gunjan <gunjankohli2004@gmail.com>
2024-11-14 13:43:17 +0530

    updateuser,getusers

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const {
-    registerUser,
-    loginUser
-}=require("../controllers/userController");
-const {JsonwebTokenError} =require("jsowebtoken");
-const { validateJwtToken } = require("../middlewares/jwtAuthMiddleware");
-router.post("/" , registerUser);
-router.post("/login",validateJwtToken, loginUser);    
-module.exports=router;
+const { validateJwtToken } = require("../middlewares/jwtAuthMiddleware");  // <-- Use require() instead of import
+const { registerUser, loginUser, getAllUsers, getUserProfile, updateUserProfile } = require("../controllers/userController");
+
+// Route to register a new user
+router.post("/register", registerUser);
+
+// Route to login an existing user
+router.post("/login", loginUser);
+
+// Protected route to get all users
+router.get("/getallusers", validateJwtToken, getAllUsers);
+
+// Route to get the user's own profile (protected)
+router.get("/myaccount", validateJwtToken, getUserProfile);
+
+// Route to update the user's profile (protected)
+router.patch("/myaccount", validateJwtToken, updateUserProfile);
+
+module.exports = router;
